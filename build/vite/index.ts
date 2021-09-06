9190cca7234d5bb1e138f7eb433fa3a879730cf2
coder_Q <1282117217@qq.com>
2021-09-06 19:52:58 +0800

    fix: 🐛解决配置错误

--- build/vite/index.ts
+++ build/vite/index.ts
@@ -1,10 +1,8 @@
 import type { Plugin } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export function createPluginVite() {
-  const vitePlugin: (Plugin | Plugin[])[] => [
-    reactRefresh()
-  ]
+  const vitePlugin: (Plugin | Plugin[])[] = [reactRefresh()];
 
-  return vitePlugin
+  return vitePlugin;
 }
